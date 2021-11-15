c78328bce95fcc5c3717ee2530a114cf706e4427
Kelvin Sing' oei <79026959+kelkip2001@users.noreply.github.com>
2021-11-15 16:14:53 +0300

    set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div>New Level SHOP12</div>
+      <div>New Level SHOP</div>
     </div>
   );
 }
